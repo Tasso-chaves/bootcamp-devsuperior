d79515aa020b58b9b79868dbdfbc203eabd53287
Tasso-chaves <tassochav@gmail.com>
2023-03-10 22:42:47 -0300

    Backend project created

---
+++ backend/democatalog/src/main/java/com/dev/democatalog/DemocatalogApplication.java
@@ -0,0 +1,13 @@
+package com.dev.democatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocatalogApplication.class, args);
+	}
+
+}
