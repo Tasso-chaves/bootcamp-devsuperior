ba763ce47b90b838ab24f420180900fdf937ae6a
Tasso-chaves <tassochav@gmail.com>
2023-03-12 21:06:12 -0300

    Created Database Seeding

---
+++ backend/democatalog/src/main/resources/import.sql
@@ -0,0 +1,22 @@
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Shirts');
+INSERT INTO tb_category (name) VALUES ('Shorts');
+INSERT INTO tb_category (name) VALUES ('Telephone');
+INSERT INTO tb_category (name) VALUES ('Clocks');
+INSERT INTO tb_category (name) VALUES ('Shoes');
+INSERT INTO tb_category (name) VALUES ('Pants');
+INSERT INTO tb_category (name) VALUES ('Electronics');
+INSERT INTO tb_category (name) VALUES ('Beauty');
+INSERT INTO tb_category (name) VALUES ('Health');
+INSERT INTO tb_category (name) VALUES ('Games');
+
+
+
+
+
+
+
+
+
+
+
