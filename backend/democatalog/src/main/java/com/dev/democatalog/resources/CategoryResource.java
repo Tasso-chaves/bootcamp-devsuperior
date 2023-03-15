d352308dce47207cc1cc3cb8983f8e68932d72c0
Tasso-chaves <tassochav@gmail.com>
2023-03-14 22:44:49 -0300

    Insert new Category

--- backend/democatalog/src/main/java/com/dev/democatalog/resources/CategoryResource.java
+++ backend/democatalog/src/main/java/com/dev/democatalog/resources/CategoryResource.java
@@ -1,12 +1,16 @@
 package com.dev.democatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dev.democatalog.dto.CategoryDTO;
 import com.dev.democatalog.services.CategoryService;
@@ -30,6 +34,16 @@ public class CategoryResource {
 
         CategoryDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping()
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(dto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
