429e5593ca64baa98220c0d8c7a4ee773cec3866
Tasso-chaves <tassochav@gmail.com>
2023-03-16 23:07:23 -0300

    Add Pagination

--- backend/democatalog/src/main/resources/import.sql
+++ backend/democatalog/src/main/resources/import.sql
@@ -9,6 +9,37 @@ INSERT INTO tb_category (name) VALUES ('Electronics');
 INSERT INTO tb_category (name) VALUES ('Beauty');
 INSERT INTO tb_category (name) VALUES ('Health');
 INSERT INTO tb_category (name) VALUES ('Games');
+INSERT INTO tb_category (name) VALUES ('balls');
+INSERT INTO tb_category (name) VALUES ('Foods');
+INSERT INTO tb_category (name) VALUES ('Home appliances');
+INSERT INTO tb_category (name) VALUES ('sports');
+INSERT INTO tb_category (name) VALUES ('TV');
+INSERT INTO tb_category (name) VALUES ('Automotive');
+INSERT INTO tb_category (name) VALUES ('Toys');
+INSERT INTO tb_category (name) VALUES ('Tools');
+INSERT INTO tb_category (name) VALUES ('Pet Shop');
+INSERT INTO tb_category (name) VALUES ('shoes');
+INSERT INTO tb_category (name) VALUES ('Desk');
+INSERT INTO tb_category (name) VALUES ('Music');
+INSERT INTO tb_category (name) VALUES ('Leisure');
+INSERT INTO tb_category (name) VALUES ('Computing');
+INSERT INTO tb_category (name) VALUES ('Perfumery');
+INSERT INTO tb_category (name) VALUES ('Suitcases');
+INSERT INTO tb_category (name) VALUES ('Gift voucher');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
